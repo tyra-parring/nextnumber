042801d867f4190c3652b1772bbc25373a14cd33
Tyra Parring <parringt@gmail.com>
2024-06-04 12:33:26 +0200

    completed and edited work

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// let currentNumber = 9;
+
+// let nextNumber = currentNumber + 1;
+// console.log("Next number:", nextNumber);
+
+let t=0
+let tyra =[20,2,6,8]
+
+function thankYouNext() {
+    if (t<tyra.length) {
+       console.log(tyra[t]); 
+    }
+    t++
+}
+thankYouNext()
+thankYouNext()
+thankYouNext()
